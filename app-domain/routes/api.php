5eb626897ca0a368f8fc1580d583c8a97601d043
NikitaShuvalov <ktototam065@gmail.com>
2025-06-19 00:54:43 +0600

    routes/api

--- app-domain/routes/api.php
+++ app-domain/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DomainCheckController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:sanctum')->post('/check-domains', [DomainCheckController::class, 'check']);
