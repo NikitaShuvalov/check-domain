245833d078503ec1342f9f7d9b3586d9d3a211ca
NikitaShuvalov <ktototam065@gmail.com>
2025-06-19 00:52:10 +0600

    Livewire/DomainChecker

--- app-domain/app/Livewire/DomainChecker.php
+++ app-domain/app/Livewire/DomainChecker.php
@@ -1,11 +1,43 @@
-<?php
-
-namespace App\Livewire;
+namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
 
 class DomainChecker extends Component
 {
+    public $input = '';
+    public $results = [];
+
+    public function checkDomains()
+    {
+        $domains = collect(preg_split('/[\n,]+/', $this->input))
+            ->map(fn($d) => trim($d))
+            ->filter()
+            ->unique();
+
+        $this->results = [];
+
+        foreach ($domains as $domain) {
+            if (!$this->isValidDomain($domain)) {
+                $this->results[$domain] = 'Невалидный домен';
+                continue;
+            }
+
+            // Фейковый WHOIS
+            if (Str::endsWith($domain, '.test')) {
+                $this->results[$domain] = 'Доступен для покупки';
+            } else {
+                $date = now()->addDays(rand(10, 365))->format('d.m.Y');
+                $this->results[$domain] = "Занят (до {$date})";
+            }
+        }
+    }
+
+    public function isValidDomain($domain): bool
+    {
+        return (bool) filter_var('http://' . $domain, FILTER_VALIDATE_URL);
+    }
+
     public function render()
     {
         return view('livewire.domain-checker');
